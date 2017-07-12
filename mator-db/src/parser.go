af29550c2a3cf6f1dc62857b51340db5da928af3
Sebastian <s.meiser@ucl.ac.uk>
2017-07-12 11:13:31 +0100

    removed redeclaration of variables

--- mator-db/src/parser.go
+++ mator-db/src/parser.go
@@ -303,11 +303,11 @@ func ParseAndInsert(month time.Time, mainFile string, complementaryFile string, 
 		// Trying to fix a weird bug in AddDate
 		nodes[keyNode][0].Published = time.Date(myyear, mymonth, myday-1, myhour, mymin, mysec, 0, loci)
 
-		loci := tmp.Location()
+		loci = tmp.Location()
 		logger.Println("got location ")
 
-		myyear, mymonth, myday := tmp.Date()
-	        myhour, mymin, mysec := tmp.Clock()
+		myyear, mymonth, myday = tmp.Date()
+	        myhour, mymin, mysec = tmp.Clock()
 	    logger.Println("i am here")
 
 		//nodes[keyNode][0].Published = nodes[keyNode][0].Published.AddDate(0, 0, -1)
