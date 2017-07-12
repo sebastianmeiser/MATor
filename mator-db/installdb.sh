77fe64c2ab496780958c365b88aff23d0bb6fbfe
Sebastian <s.meiser@ucl.ac.uk>
2017-07-12 13:26:57 +0100

    paths

--- mator-db/installdb.sh
+++ mator-db/installdb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPTPATH = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p go/bin
 mkdir -p go/pkg
 mkdir -p go/src/mator-db
@@ -7,7 +9,7 @@ mkdir -p go/src/mator-db
 cp src/*.go go/src/mator-db/
 cp -r src/asn go/src/mator-db/
 #export GOPATH=`pwd`/go
-export GOPATH='/usr/bin/go'
+export GOPATH="${SCRIPTPATH}/go"
 cd go/src/mator-db/
 go get .
 go install
